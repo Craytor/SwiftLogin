73cd637e539268f448ca5967c4e665d557b6de11
Tyler Youschak <tyleryouschak@gmail.com>
2015-10-14 22:35:01 -0400

    Few Final Touches

--- SwiftLogin/RegisterViewController.swift
+++ SwiftLogin/RegisterViewController.swift
@@ -43,39 +43,37 @@ class RegisterPageViewController: UIViewController {
         }
         
         // Check if passwords are equal
-        if(userPassword != userRepeatPassword) {
-            // Display an alert message
-            displayMyAlertMessage("Passwords do not match");
+        if(userPassword == userRepeatPassword) {
+            // Passwords are the same, so save that data
+            let userDefaults = NSUserDefaults.standardUserDefaults()
+            userDefaults.setValue(userEmail, forKey: "userEmail")
+            userDefaults.setValue(userPassword, forKey: "userPassword")
+            userDefaults.synchronize()
+            
+            let myAlert = UIAlertController(title:"Alert", message:"Registration is successful. Thank you!", preferredStyle: UIAlertControllerStyle.Alert);
+            
+            let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.Default){ action in
+                self.dismissViewControllerAnimated(true, completion:nil);
+            }
+            
+            myAlert.addAction(okAction);
+            self.presentViewController(myAlert, animated:true, completion:nil);
+            
+            return;
+
+        } else {
+            // Passwords are not equal so display an alert message
+            displayMyAlertMessage("Your passwords provided do not match, please try again.");
             return;
             
         }
-
-        let userDefaults = NSUserDefaults.standardUserDefaults()
-        userDefaults.setValue(userEmail, forKey: "userEmail")
-        userDefaults.setValue(userPassword, forKey: "userPassword")
-        userDefaults.synchronize() // don't forget this!!!!
-
-
-  
-        var myAlert = UIAlertController(title:"Alert", message:"Registration is successful. Thank you!", preferredStyle: UIAlertControllerStyle.Alert);
-
-        let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.Default){ action in
-            self.dismissViewControllerAnimated(true, completion:nil);
-        }
-
-        myAlert.addAction(okAction);
-        self.presentViewController(myAlert, animated:true, completion:nil);
-        
-        return;
-            
-            
         
     }
     
     
     func displayMyAlertMessage(userMessage:String) {
         
-        let myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle: UIAlertControllerStyle.Alert);
+        let myAlert = UIAlertController(title:"Whoops!", message:userMessage, preferredStyle: UIAlertControllerStyle.Alert);
         
         let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.Default, handler:nil);
         
